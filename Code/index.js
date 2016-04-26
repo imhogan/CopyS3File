83abebbb351c8aaa3ecbfe9577820cb871efc284
Ian Hogan <ian.hogan@thinkronicity.com.au>
2016-04-26 13:00:11 +1000

    Updated notes.

--- Code/index.js
+++ Code/index.js
@@ -1,10 +1,17 @@
 /**
- * A sample Lambda function that takes an AWS CloudFormation stack name
- * and returns the outputs from that stack. Make sure to include permissions
- * for `cloudformation:DescribeStacks` in your execution role!
+ * Node.js Lambda backed AWS CLoudFormation Custom Resource to copy an S3 file.
  *
- * See http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/walkthrough-custom-resources-lambda-cross-stack-ref.html
- * for documentation on how to use this blueprint.
+ * When deploying an AWS stack with CloudFormation, it can be helpful to copy a particular configuration file, eg perhaps you have
+ * dev, uat and prod versions of a config file, to a target file. The following CloudFormation template demonstrates this:
+ * 
+ * https://s3-ap-northeast-1.amazonaws.com/au-com-thinkronicity-opencode-apne1/au-com-thinkronicity-CopyS3File-Sample.template
+ * 
+ * This template loads the code from a bucket in the AWS region the Stack is being deployed in. 
+ * 
+ * These buckets are readable by any AWS account, and are set to User Pays mode.
+ * 
+ * Ian.Hogan@THINKronicity.com.au
+ *
  */
 console.log('Loading function');
 
